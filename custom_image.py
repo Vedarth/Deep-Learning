31c1cbce67048f96f91898d70a5200f134caee7a
Vedarth <vedarth.sharma@gmail.com>
2017-09-10 12:20:04 +0530

    add custom_image.py

---
+++ custom_image.py
@@ -0,0 +1,13 @@
+## START CODE HERE ## (PUT YOUR IMAGE NAME) 
+my_image = "la_defense.jpg"   # change this to the name of your image file 
+## END CODE HERE ##
+
+# We preprocess the image to fit your algorithm.
+fname = "images/" + my_image
+image = np.array(ndimage.imread(fname, flatten=False))
+my_image = scipy.misc.imresize(image, size=(num_px,num_px)).reshape((1, num_px*num_px*3)).T
+my_predicted_image = predict(d["w"], d["b"], my_image)
+
+plt.imshow(image)
+print(d["w"], d["b"])
+print("y = " + str(np.squeeze(my_predicted_image)) + ", your algorithm predicts a \"" + classes[int(np.squeeze(my_predicted_image)),].decode("utf-8") +  "\" picture.")
