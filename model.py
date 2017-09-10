c286dd9c6d788ca461503ffa6e96e1d998f677e1
Vedarth <vedarth.sharma@gmail.com>
2017-09-10 12:17:20 +0530

    add plot

--- model.py
+++ model.py
@@ -48,9 +48,16 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations, learning_rate, print
     
     return d
 
-#d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 50000, learning_rate = 0.0001, print_cost = True)
+d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 50000, learning_rate = 0.0001, print_cost = True)
 """
 index = 1
 plt.imshow(test_set_x[:,index].reshape((num_px, num_px, 3)))
 print ("y = " + str(test_set_y[0,index]) + ", you predicted that it is a \"" + classes[d["Y_prediction_test"][0,index]].decode("utf-8") +  "\" picture.")
 """
+# Plot learning curve (with costs)
+costs = np.squeeze(d['costs'])
+plt.plot(costs)
+plt.ylabel('cost')
+plt.xlabel('iterations (per hundreds)')
+plt.title("Learning rate =" + str(d["learning_rate"]))
+plt.show()
