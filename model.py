dae557304691e611ceb1250c4b3ce73b2aca9a06
Vedarth <vedarth.sharma@gmail.com>
2017-09-10 12:16:34 +0530

    add error

--- model.py
+++ model.py
@@ -49,3 +49,8 @@ def model(X_train, Y_train, X_test, Y_test, num_iterations, learning_rate, print
     return d
 
 #d = model(train_set_x, train_set_y, test_set_x, test_set_y, num_iterations = 50000, learning_rate = 0.0001, print_cost = True)
+"""
+index = 1
+plt.imshow(test_set_x[:,index].reshape((num_px, num_px, 3)))
+print ("y = " + str(test_set_y[0,index]) + ", you predicted that it is a \"" + classes[d["Y_prediction_test"][0,index]].decode("utf-8") +  "\" picture.")
+"""
