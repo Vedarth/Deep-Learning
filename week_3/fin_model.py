bd5151890eb94165289473ec00dd03052100bee0
Vedarth <vedarth.sharma@gmail.com>
2017-10-01 23:39:57 +0530

    add final model

---
+++ week_3/fin_model.py
@@ -0,0 +1,6 @@
+# Build a model with a n_h-dimensional hidden layer
+parameters = nn_model(X, Y, n_h = 4, num_iterations = 10000, print_cost=True)
+
+# Plot the decision boundary
+plot_decision_boundary(lambda x: predict(parameters, x.T), X, Y)
+plt.title("Decision Boundary for hidden layer size " + str(4))
