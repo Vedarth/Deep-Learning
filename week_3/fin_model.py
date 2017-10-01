d15d6fdeb41e375abda5c6786206a5cabeb90d25
Vedarth <vedarth.sharma@gmail.com>
2017-10-01 23:40:36 +0530

    add accuracy calculator model

--- week_3/fin_model.py
+++ week_3/fin_model.py
@@ -4,3 +4,8 @@ parameters = nn_model(X, Y, n_h = 4, num_iterations = 10000, print_cost=True)
 # Plot the decision boundary
 plot_decision_boundary(lambda x: predict(parameters, x.T), X, Y)
 plt.title("Decision Boundary for hidden layer size " + str(4))
+
+
+# Print accuracy
+predictions = predict(parameters, X)
+print ('Accuracy: %d' % float((np.dot(Y,predictions.T) + np.dot(1-Y,1-predictions.T))/float(Y.size)*100) + '%')
