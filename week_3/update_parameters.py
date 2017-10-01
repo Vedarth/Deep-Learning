00fbd9acb5b7048b3c4d774aa4fd8ccb3d235629
Vedarth <vedarth.sharma@gmail.com>
2017-10-01 23:34:47 +0530

    add tests for update_parameters

--- week_3/update_parameters.py
+++ week_3/update_parameters.py
@@ -38,3 +38,10 @@ def update_parameters(parameters, grads, learning_rate = 1.2):
                   "b2": b2}
     
     return parameters
+def main():
+    parameters, grads = update_parameters_test_case()
+    parameters = update_parameters(parameters, grads)
+    print("W1 = " + str(parameters["W1"]))
+    print("b1 = " + str(parameters["b1"]))
+    print("W2 = " + str(parameters["W2"]))
+    print("b2 = " + str(parameters["b2"]))
