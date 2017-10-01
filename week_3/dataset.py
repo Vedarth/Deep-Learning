0bdcc27fbf8933115f775573d7d3644ec82cc36d
Vedarth <vedarth.sharma@gmail.com>
2017-10-01 23:12:58 +0530

    Add dataset

---
+++ week_3/dataset.py
@@ -0,0 +1,13 @@
+X, Y = load_planar_dataset()
+# Visualize the data:
+plt.scatter(X[0, :], X[1, :], c=Y, s=40, cmap=plt.cm.Spectral);
+
+### START CODE HERE ### (≈ 3 lines of code)
+shape_X = np.shape(X)
+shape_Y = np.shape(Y)
+m = X[1].shape  # training set size
+### END CODE HERE ###
+
+print ('The shape of X is: ' + str(shape_X))
+print ('The shape of Y is: ' + str(shape_Y))
+print ('I have m = %d training examples!' % (m))
