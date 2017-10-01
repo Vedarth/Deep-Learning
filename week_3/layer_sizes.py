6b305633213e1e661fb4f4669be1afa95fa7f6b3
Vedarth <vedarth.sharma@gmail.com>
2017-10-01 23:17:02 +0530

    update layer_sizes.py

--- week_3/layer_sizes.py
+++ week_3/layer_sizes.py
@@ -15,3 +15,11 @@ def layer_sizes(X, Y):
     n_y = np.shape(Y)[0] # size of output layer
     ### END CODE HERE ###
     return (n_x, n_h, n_y)
+
+
+def main():
+    X_assess, Y_assess = layer_sizes_test_case()
+    (n_x, n_h, n_y) = layer_sizes(X_assess, Y_assess)
+    print("The size of the input layer is: n_x = " + str(n_x))
+    print("The size of the hidden layer is: n_h = " + str(n_h))
+    print("The size of the output layer is: n_y = " + str(n_y))
