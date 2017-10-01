9591a589216df7fb230f4e8cd3268409cc56ede7
Vedarth <vedarth.sharma@gmail.com>
2017-10-01 23:19:33 +0530

    update initialization

--- week_3/initialize_parameters.py
+++ week_3/initialize_parameters.py
@@ -33,3 +33,12 @@ def initialize_parameters(n_x, n_h, n_y):
                   "b2": b2}
     
     return parameters
+
+def main():
+    n_x, n_h, n_y = initialize_parameters_test_case()
+
+    parameters = initialize_parameters(n_x, n_h, n_y)
+    print("W1 = " + str(parameters["W1"]))
+    print("b1 = " + str(parameters["b1"]))
+    print("W2 = " + str(parameters["W2"]))
+    print("b2 = " + str(parameters["b2"]))
