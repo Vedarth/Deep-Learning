37665f719dc57141ec2d1ea333bcafeadfe24057
Vedarth <vedarth.sharma@gmail.com>
2017-10-01 23:27:54 +0530

    add return variable for backward propagation

--- week_3/backward_propagation.py
+++ week_3/backward_propagation.py
@@ -33,3 +33,9 @@ def backward_propagation(parameters, cache, X, Y):
     dW1 = (1/m) * (np.dot(dZ1,X.T))
     db1 = np.sum(dZ1,axis = 1,keepdims = True)/m
     ### END CODE HERE ###
+    grads = {"dW1": dW1,
+             "db1": db1,
+             "dW2": dW2,
+             "db2": db2}
+    
+    return grads
