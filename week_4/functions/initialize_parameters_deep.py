02125faeaa79feb1f5dc9e1a2b1d91b471559310
Vedarth <vedarth.sharma@gmail.com>
2017-10-02 20:27:46 +0530

    update deep initialization test file week4 helper

--- week_4/functions/initialize_parameters_deep.py
+++ week_4/functions/initialize_parameters_deep.py
@@ -24,3 +24,9 @@ def initialize_parameters_deep(layer_dims):
 
         
     return parameters
+def test():
+    parameters = initialize_parameters_deep([5,4,3])
+    print("W1 = " + str(parameters["W1"]))
+    print("b1 = " + str(parameters["b1"]))
+    print("W2 = " + str(parameters["W2"]))
+    print("b2 = " + str(parameters["b2"]))
