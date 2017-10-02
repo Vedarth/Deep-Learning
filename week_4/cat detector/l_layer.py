0d71ab18c70fb1d7b23ed5a13d73ea07c7e60c2c
Vedarth <vedarth.sharma@gmail.com>
2017-10-03 01:34:13 +0530

    add test for l layer model for week4 application

--- week_4/cat detector/l_layer.py
+++ week_4/cat detector/l_layer.py
@@ -61,3 +61,10 @@ def L_layer_model(X, Y, layers_dims, learning_rate = 0.0075, num_iterations = 30
     plt.show()
 
     return parameters
+
+def test():
+    parameters = L_layer_model(train_x, train_y, layers_dims, num_iterations = 2500, print_cost = True)
+
+pred_train = predict(train_x, train_y, parameters)
+
+pred_test = predict(test_x, test_y, parameters)
