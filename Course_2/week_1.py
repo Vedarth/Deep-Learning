bc5331321f1e9504f67480eb4517515f1c637939
Vedarth <vedarth.sharma@gmail.com>
2017-10-29 15:13:51 +0530

    Add initialize with zeros

--- Course_2/week_1.py
+++ Course_2/week_1.py
@@ -75,3 +75,31 @@ def model(X, Y, learning_rate = 0.01, num_iterations = 15000, print_cost = True,
     plt.show()
     
     return parameters
+
+
+
+
+
+def initialize_parameters_zeros(layers_dims):
+    """
+    Arguments:
+    layer_dims -- python array (list) containing the size of each layer.
+    
+    Returns:
+    parameters -- python dictionary containing your parameters "W1", "b1", ..., "WL", "bL":
+                    W1 -- weight matrix of shape (layers_dims[1], layers_dims[0])
+                    b1 -- bias vector of shape (layers_dims[1], 1)
+                    ...
+                    WL -- weight matrix of shape (layers_dims[L], layers_dims[L-1])
+                    bL -- bias vector of shape (layers_dims[L], 1)
+    """
+    
+    parameters = {}
+    L = len(layers_dims)            # number of layers in the network
+    
+    for l in range(1, L):
+        ### START CODE HERE ### (≈ 2 lines of code)
+        parameters['W' + str(l)] = np.zeros((layers_dims[1], layers_dims[0]))
+        parameters['b' + str(l)] = np.zeros((layers_dims[1], 1))
+        ### END CODE HERE ###
+    return parameters
