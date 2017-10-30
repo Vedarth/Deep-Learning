d8cca2e0f5fd156defa84e7986a350ab0579f4d2
Vedarth <vedarth.sharma@gmail.com>
2017-10-30 23:20:46 +0530

    Add backward prob

--- Course_2/gradient_check.py
+++ Course_2/gradient_check.py
@@ -27,3 +27,22 @@ x, theta = 2, 4
 J = forward_propagation(x, theta)
 print ("J = " + str(J))
 
+
+
+def backward_propagation(x, theta):
+    """
+    Computes the derivative of J with respect to theta (see Figure 1).
+    
+    Arguments:
+    x -- a real-valued input
+    theta -- our parameter, a real number as well
+    
+    Returns:
+    dtheta -- the gradient of the cost with respect to theta
+    """
+    
+    ### START CODE HERE ### (approx. 1 line)
+    dtheta = x
+    ### END CODE HERE ###
+    
+    return dtheta
