0b5393a70cb697b77fc19bb3e2d599bb1feedff3
Vedarth <vedarth.sharma@gmail.com>
2017-11-02 21:40:22 +0530

    Add linear function.py

---
+++ Course_2/week_3/linear_function.py
@@ -0,0 +1,32 @@
+# GRADED FUNCTION: linear_function
+
+def linear_function():
+    """
+    Implements a linear function: 
+            Initializes W to be a random tensor of shape (4,3)
+            Initializes X to be a random tensor of shape (3,1)
+            Initializes b to be a random tensor of shape (4,1)
+    Returns: 
+    result -- runs the session for Y = WX + b 
+    """
+    
+    np.random.seed(1)
+    
+    ### START CODE HERE ### (4 lines of code)
+    X = np.random.randn(3, 1)
+    W = np.random.randn(4, 3)
+    b = np.random.randn(4, 1)
+    Y = tf.add(tf.matmul(W, X), b)
+    ### END CODE HERE ### 
+    
+    # Create the session using tf.Session() and run it with sess.run(...) on the variable you want to calculate
+    
+    ### START CODE HERE ###
+    sess = tf.Session()
+    result = sess.run(Y)
+    ### END CODE HERE ### 
+    
+    # close the session 
+    sess.close()
+
+    return result
