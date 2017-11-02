d218595667585b78a2dbc601d85e9233aafed3cf
Vedarth <vedarth.sharma@gmail.com>
2017-11-02 21:55:40 +0530

    Add file to predict user provided image using trained parameters

---
+++ Course_2/week_3/own_image.py
@@ -0,0 +1,16 @@
+import scipy
+from PIL import Image
+from scipy import ndimage
+
+## START CODE HERE ## (PUT YOUR IMAGE NAME) 
+my_image = "thumbs_up.jpg"
+## END CODE HERE ##
+
+# We preprocess your image to fit your algorithm.
+fname = "images/" + my_image
+image = np.array(ndimage.imread(fname, flatten=False))
+my_image = scipy.misc.imresize(image, size=(64,64)).reshape((1, 64*64*3)).T
+my_image_prediction = predict(my_image, parameters)
+
+plt.imshow(image)
+print("Your algorithm predicts: y = " + str(np.squeeze(my_image_prediction)))
